502cc0e436b9fb7b289e58d3c647062289e20d45
Fadil Fahrduin <fadilfahrudin32@gmail.com>
2022-11-10 08:24:57 +0700

    [update] 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import Routes from "./config/Router";
+import "./style/Tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Header } from "../../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <h1 className="text-4xl">ini adalah testing</h1>
+    </div>
+  );
 };
 
 export default Home;
