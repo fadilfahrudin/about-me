c030899b0c6fc6823e3d6aac421b3b0ef46f5df2
initial-F <62017779+fadilfahrudin@users.noreply.github.com>
2022-12-28 14:05:34 +0700

    ke 3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-4xl">ini adalah home ke-2</h1>
+      <h1 className="text-4xl">ini adalah home ke-3</h1>
     </div>
   );
 };
